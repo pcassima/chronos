39d1e346cf3bd05f9aaf83a7d700f201db4b4b48
Pieter-Jan Cassiman <fenixonecomputers@gmail.com>
2017-10-26 16:43:26 +0200

    Update Chronos.cpp

--- Chronos/Chronos.cpp
+++ Chronos/Chronos.cpp
@@ -1,32 +1,100 @@
+/*
+ * Library for the Chronos V1 shield
+ * Made by Fenix Computers
+ * Library written by Pieter-Jan
+ * Version V1.0
+ */
 #include "Chronos.h"
-/*
-int SER = 9;    //INPUT
-int OE = 10;    //blank display on high
-int RCLK = 11;  //Latch clock
-int SRCLK = 12; //Shift clock
-int SRCLR = 13; //RESET on low
-int digit0 = 8; //digit select Bit 0 > digits
-int digit1 = 7; //digit select Bit 1 > digits
-int digit2 = 6; //digit select bit 2 > dots
-/*
-int sw1 = 2;
-int sw2 = 3;
-int sw3 = 4;
-int sw4 = 5;
-unsigned long Time;
-int ticker = 0;
+#include <Arduino.h>
 
-byte brightness = 255;
+const int SER = 9;    //INPUT
+const int OE = 10;    //blank display on high
+const int RCLK = 11;  //Latch clock
+const int SRCLK = 12; //Shift clock
+const int SRCLR = 13; //RESET on low
 
-byte digits[] = {0b11111100, 0b01100000, 0b11011010, 0b11110010, 0b01100110, 0b10110110, 0b10111110, 0b11100000, 0b11111110, 0b11110110};
-byte digitsDP[] = {0b11111101, 0b01100001, 0b11011011, 0b11110011, 0b01100111, 0b10110111, 0b10111111, 0b11100001, 0b11111111, 0b11110111};
-*/
+const int digit0 = 8; //digit select Bit 0 > digits
+const int digit1 = 7; //digit select Bit 1 > digits
+const int digit2 = 6; //digit select bit 2 > dots
 
-Chronos::Chronos() {
-  //Anything you need when instantiating your object goes here
+int Brightness = 255;
+
+const byte digits[] = {0b11111100, 0b01100000, 0b11011010, 0b11110010, 0b01100110, 0b10110110, 0b10111110, 0b11100000, 0b11111110, 0b11110110, 0b11000000, 0b00000000};
+
+Chronos::Chronos(){
+    //Anything you need when instantiating your object goes here
+    pinMode(SER, OUTPUT);
+    pinMode(OE, OUTPUT);
+    pinMode(RCLK, OUTPUT);
+    pinMode(SRCLK, OUTPUT);
+    pinMode(SRCLR, OUTPUT);
+
+    pinMode(digit0, OUTPUT);
+    pinMode(digit1, OUTPUT);
+    pinMode(digit2, OUTPUT);
+
+    digitalWrite(OE, LOW);
+    digitalWrite(SRCLR, HIGH);
+
+    digitalWrite(digit0, LOW);
+    digitalWrite(digit1, LOW);
+    digitalWrite(digit2, LOW);
 }
-Chronos::~Chronos(){
-  
+
+Chronos::~Chronos(){}
+
+void Chronos::writeInt(int number){
+    int digit0Value = 0;
+    int digit1Value = 0;
+    int digit2Value = 0;
+    int digit3Value = 0;
+
+    digit3Value = number % 10;
+    number = number - digit3Value;
+    
+    digit2Value = (number % 100) / 10;
+    number = number - digit2Value;
+
+    digit1Value = (number % 1000) / 100;
+    number = number - digit1Value;
+
+    digit0Value = (number % 10000) / 1000;
+
+    writeDigit(digits[digit0Value],0);
+    writeDigit(digits[digit1Value],1);
+    writeDigit(digits[digit2Value],2);
+    writeDigit(digits[digit3Value],3);
+
 }
 
+void Chronos::shiftOutShape(byte shape){
+    for (byte i = 0; i < 8; i++){
+        digitalWrite(SER, (shape >> i) & 1);
 
+        digitalWrite(SRCLK, HIGH);
+        digitalWrite(SRCLK, LOW);
+    }
+    digitalWrite(SER, LOW);
+}
+
+void Chronos::chooseDigit(byte digit){
+    digitalWrite(digit0, digit & 1);
+    digitalWrite(digit1, digit & 2);
+    digitalWrite(digit2, digit & 4);
+}
+
+void Chronos::writeDigit(byte shape, byte digit){
+    shiftOutShape(shape);
+    chooseDigit(digit);
+    digitalWrite(RCLK, HIGH);
+    digitalWrite(RCLK, LOW);
+
+    digitalWrite(OE,LOW);
+    delayMicroseconds(Brightness);
+    digitalWrite(OE, HIGH);
+}
+
+void Chronos::setBrightness(int newBrightness){
+    Brightness = newBrightness;
+    
+}
