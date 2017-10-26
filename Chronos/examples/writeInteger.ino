43638c81cc0294cfc48d9dc6c6223d46e3875ba7
Pieter-Jan Cassiman <fenixonecomputers@gmail.com>
2017-10-26 16:51:38 +0200

    Create writeInteger.ino

---
+++ Chronos/examples/writeInteger.ino
@@ -0,0 +1,25 @@
+/*
+ * Example showing the capabilities of the
+ * writeInt and setBrightness functions
+ * Written by Pieter-Jan Cassiman
+ */
+
+#include <Chronos.h>
+
+int number = 2017; //the number that we want to display
+
+Chronos chronos; //initialising the library
+
+
+void setup() {
+  // put your setup code here, to run once:
+  //Setting the brightness of the library to the maximum
+  //value
+    chronos.setBrightness(1023);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  //Writing the number on the display
+    chronos.writeInt(number);
+}
